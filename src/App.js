637ea24cfaddc6c242e75d2c54289801547df66b
Robson Gomes <robson.080490@gmail.com>
2021-10-15 13:48:35 -0300

    primeiro comentario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [ itens, setItens ] = useState([])
+  const [itensPerPage, setItensPerPage] = useState(10)
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const pages = Math.ceil(itens.length / itensPerPage)
+  const startIndex = currentPage * itensPerPage;
+  const endIndex = startIndex + itensPerPage;
+  const currentItens = itens.slice(startIndex, endIndex)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(data => data)
+
+      setItens(result)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {Array.from(Array(pages), (item, index) => {
+          return <button>{index}</button>
+        })}
+      </div>
+
+      {currentItens.map(item => {
+        return <div><spam>{item.id}<span>{item.title}<spam>{item.completed}</spam></span></spam></div>
+      })}
     </div>
   );
 }
